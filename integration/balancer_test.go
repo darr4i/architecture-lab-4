1e4396289036aa155b7331012a81ba21b2173d0c
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2024-05-24 17:07:42 +0300

    added integration tests

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -19,14 +19,22 @@ func TestBalancer(t *testing.T) {
 		t.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+	for i := 0; i < 5; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		if err != nil {
+			t.Error(err)
+		}
+		t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+		resp.Body.Close()
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
 }
 
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+	for i := 0; i < b.N; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		if err != nil {
+			b.Error(err)
+		}
+		resp.Body.Close()
+	}
 }
